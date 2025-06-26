2071e33b77a7e2fa5693c1100de3f2ad5b7c144a
abrilamoedo <114035596+abrilalvarez@users.noreply.github.com>
2025-06-25 21:27:13 -0300

    Update RepositorioOficina.java

--- sistemas/src/main/java/com/crud/sistemas/repositorio/RepositorioOficina.java
+++ sistemas/src/main/java/com/crud/sistemas/repositorio/RepositorioOficina.java
@@ -3,5 +3,6 @@ package com.crud.sistemas.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.crud.sistemas.modelo.Oficina;
 
+@Repository
 public interface RepositorioOficina extends JpaRepository<Oficina, Integer> {
 }
