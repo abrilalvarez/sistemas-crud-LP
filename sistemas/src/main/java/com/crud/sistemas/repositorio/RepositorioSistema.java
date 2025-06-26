01fe24cdb8c5cfd014828559efa08f6777cd5bf3
abrilamoedo <114035596+abrilalvarez@users.noreply.github.com>
2025-06-25 21:27:38 -0300

    Update RepositorioSistema.java

--- sistemas/src/main/java/com/crud/sistemas/repositorio/RepositorioSistema.java
+++ sistemas/src/main/java/com/crud/sistemas/repositorio/RepositorioSistema.java
@@ -3,6 +3,7 @@ package com.crud.sistemas.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.crud.sistemas.modelo.Sistema;
 
+@Repository
 public interface RepositorioSistema extends JpaRepository<Sistema, Integer> {
 }
 
